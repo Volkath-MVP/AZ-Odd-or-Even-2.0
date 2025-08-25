7b13736ba5595098e5576c68028db389638af1be
Volkath-MVP <aquilesvolkath71@gmail.com>
2025-08-25 11:17:56 -0300

    Português: Criação dos dois botões e futuramente as funções - English: Creation of both buttons and, later on, the functions

--- AZ_Odd_or_Even2.0.py
+++ AZ_Odd_or_Even2.0.py
@@ -14,6 +14,12 @@ font_small = pg.font.SysFont("Arial", 18)
 odd_button = pg.Rect(500 * 0.25 - 75, 300 * 0.60, 150, 50)
 BRANCO = (255, 255, 255)
 VERMELHO = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+#Tamanhos de fonte para serem usados no jogo.
+font_small = pg.font.SysFont("Arial", 18)
+font_medium = pg.font.SysFont("Arial", 30)
+font_large = pg.font.SysFont("Arial", 50)
 running = True
 #random Number Generator
 number=rd.randint(1, 99)
@@ -23,18 +29,30 @@ def update_number():
     number = rd.randint(1, 99)
 def draw_game():
     #draw background and button
-    root.fill(BRANCO)
-    pg.draw.rect(root, VERMELHO, odd_button)
+    root.fill(BLACK)
     number_text = font_small.render(str(number), True, (0, 0, 0))
     root.blit(number_text, (Init_Width // 2 - number_text.get_width() // 2, Init_Height // 2 - number_text.get_height() // 2))
+    odd_button = pg.Rect(1000 * 0.25 - 75, 70 * 0.60, 150, 50)
+    even_button = pg.Rect(200 * 0.75 - 75, 70 * 0.60, 150, 50)
+    pg.draw.rect(root, GREEN, odd_button)
+    pg.draw.rect(root, GREEN, even_button)
+    odd_label = font_small.render("Impar (X)", True, BLACK)
+    even_label = font_small.render("Par (B)", True, BLACK)
+    root.blit(odd_label, odd_button.move(25, 10))
+    root.blit(even_label, even_button.move(25, 10))
     pg.display.flip()
+    return odd_button, even_button
 #loop
 while running:
-    draw_game()
+    #draw_game()
+    odd_btn, even_btn = draw_game()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
         elif event.type == pg.MOUSEBUTTONDOWN:
-            if odd_button.collidepoint(event.pos):
+            x, y = event.pos
+            if odd_btn.collidepoint((x, y)):
+                running = False
+            if even_btn.collidepoint((x, y)):
                 running = False
 pg.quit()
